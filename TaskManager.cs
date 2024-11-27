a0cf01576ca10d877ba9902150dea49214afb287
dc91_ <dc91_@hotmail.com>
2024-11-27 12:35:52 +0100

    added filepath

--- TaskManager.cs
+++ TaskManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,8 @@ namespace ToDoLy
 {
     internal class TaskManager
     {
-        //Add the file
+        private string filePath = "tasks.csv";
+        
 
         public void AddTask()
         {
